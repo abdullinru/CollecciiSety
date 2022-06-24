f96813785d1f685f3de254e01ccfa5c650f1fbaa
abdullinru <abdullin17021989@gmail.com>
2022-06-24 14:52:12 +0600

    добавил класс Employee и EmployeeService

---
+++ src/main/java/com/example/collecciisety/Employee.java
@@ -0,0 +1,48 @@
+package com.example.collecciisety;
+
+import java.util.Objects;
+
+public class Employee {
+    private String firstName;
+    private String lastName;
+
+    public Employee(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Сотрудник: %s %s \n", firstName, lastName);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Employee employee = (Employee) o;
+
+        return Objects.equals(firstName, employee.getFirstName())&&
+                Objects.equals(lastName, employee.getLastName());
+    }
+
+    @Override
+    public int hashCode() {
+        int result = firstName != null ? firstName.hashCode() : 0;
+        result = 31 * result + (lastName != null ? lastName.hashCode() : 0);
+        return result;
+    }
+}
+
+
+
+
---
+++ src/main/java/com/example/collecciisety/EmployeeService.java
@@ -0,0 +1,8 @@
+package com.example.collecciisety;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmployeeService {
+    Employee[] employees;
+}
