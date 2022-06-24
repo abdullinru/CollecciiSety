46fcf90ffaecad9796a65ab14c4ec6c2fd8dcd3e
abdullinru <abdullin17021989@gmail.com>
2022-06-24 14:24:18 +0600

    Initial commit

---
+++ src/main/java/com/example/collecciisety/CollecciiSetyApplication.java
@@ -0,0 +1,13 @@
+package com.example.collecciisety;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollecciiSetyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CollecciiSetyApplication.class, args);
+    }
+
+}
