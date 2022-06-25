7078b413dd942373e7f35cf1c670393f3c9ec172
abdullinru <abdullin17021989@gmail.com>
2022-06-25 17:28:02 +0600

    Исправил методы в сервисе. Написал методы в контроллере

---
+++ src/main/java/com/example/collecciisety/EmployeeController.java
@@ -0,0 +1,48 @@
+package com.example.collecciisety;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/employee/")
+
+public class EmployeeController {
+    private final EmployeeService employeeService;
+
+    public EmployeeController(EmployeeService employeeService) {
+        this.employeeService = employeeService;
+    }
+
+    @GetMapping("add")
+    public Employee addEmployee(@RequestParam(name = "firstName") String firstName,
+                                 @RequestParam(name = "lastName") String lastName) {
+        try {
+            return employeeService.add(firstName, lastName);
+        } catch (EmployeeStorageIsFullException | EmployeeAlreadyAddedException e) {
+            System.out.println(e.getMessage());
+        }
+        return null;
+    }
+    @GetMapping("remove")
+    public Employee removeEmployee(@RequestParam(name = "firstName") String firstName,
+                                 @RequestParam(name = "lastName") String lastName) {
+        try {
+            return employeeService.delete(firstName, lastName);
+        } catch (EmployeeNotFoundException e) {
+            System.out.println(e.getMessage());
+        }
+        return null;
+    }
+    @GetMapping("find")
+    public Employee findEmployee(@RequestParam(name = "firstName") String firstName,
+                                 @RequestParam(name = "lastName") String lastName) {
+        try {
+            return employeeService.find(firstName, lastName);
+        } catch (EmployeeNotFoundException e) {
+            System.out.println(e.getMessage());
+        }
+        return null;
+    }
+}
--- src/main/java/com/example/collecciisety/EmployeeService.java
+++ src/main/java/com/example/collecciisety/EmployeeService.java
@@ -6,46 +6,63 @@ import java.util.Objects;
 
 @Service
 public class EmployeeService {
-    Employee[] employees;
+    private Employee[] employees;
 
-//    public EmployeeService(Employee[] employees) {
-//        this.employees = employees;
-//    }
+    public EmployeeService() {
+        this.employees = new Employee[3];;
+    }
 
-    public void add(String firstName, String lastName) {
-        if (find(firstName, lastName)) {
+    public Employee add(String firstName, String lastName) {
+        if (findPrivate(firstName, lastName)) {
             throw new EmployeeAlreadyAddedException("Данный сотрудник уже добавлен! ");
         }
         for (int i = 0; i < employees.length; i++) {
             if (employees[i] == null) {
                 employees[i] = new Employee(firstName, lastName);
-                break;
+                return employees[i];
             }
         }
         throw new EmployeeStorageIsFullException("Массив переполнен! ");
 
     }
 
-    public void delete(String firstName, String lastName) {
-        if (find(firstName, lastName)) {
+    public Employee delete(String firstName, String lastName) {
+        if (!findPrivate(firstName, lastName)) {
             throw new EmployeeNotFoundException("Нет такого сотрудника");
         }
         for (int i = 0; i < employees.length; i++) {
             if (Objects.equals(employees[i].getFirstName(), firstName) &&
                     Objects.equals(employees[i].getLastName(), lastName)) {
+                Employee employee1 = new Employee(firstName, lastName);
                 employees[i] = null;
-                break;
+                return employee1;
             }
         }
+        return null;
     }
 
-    public boolean find(String firstName, String lastName) {
+    public Employee find(String firstName, String lastName) {
         for (Employee employee : employees) {
+            if (employee == null) {
+                throw new EmployeeNotFoundException("Сотрудник не найден");
+            }
+            if (Objects.equals(employee.getFirstName(), firstName) &&
+                    Objects.equals(employee.getLastName(), lastName)) {
+                return employee;
+            }
+        }
+        throw new EmployeeNotFoundException("Сотрудник не найден");
+    }
+    private boolean findPrivate(String firstName, String lastName) {
+        for (Employee employee : employees) {
+            if (employee == null) {
+                return false;
+            }
             if (Objects.equals(employee.getFirstName(), firstName) &&
                     Objects.equals(employee.getLastName(), lastName)) {
                 return true;
             }
         }
-        throw new EmployeeNotFoundException("Сотрудник не найден");
+        return false;
     }
 }
