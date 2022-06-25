7e25ef91324e72a8b6346202b121d594b41e05c9
abdullinru <abdullin17021989@gmail.com>
2022-06-25 11:16:52 +0600

    Написал методы add, delete и find

--- src/main/java/com/example/collecciisety/EmployeeService.java
+++ src/main/java/com/example/collecciisety/EmployeeService.java
@@ -2,7 +2,43 @@ package com.example.collecciisety;
 
 import org.springframework.stereotype.Service;
 
+import java.util.Objects;
+
 @Service
 public class EmployeeService {
     Employee[] employees;
+
+//    public EmployeeService(Employee[] employees) {
+//        this.employees = employees;
+//    }
+
+    public void add(String firstName, String lastName) {
+        for (int i = 0; i < employees.length; i++) {
+            if (employees[i] == null) {
+                employees[i] = new Employee(firstName, lastName);
+                break;
+            }
+        }
+
+    }
+
+    public void delete(String firstName, String lastName) {
+        for (int i = 0; i < employees.length; i++) {
+            if (Objects.equals(employees[i].getFirstName(), firstName) &&
+                    Objects.equals(employees[i].getLastName(), lastName)) {
+                employees[i] = null;
+                break;
+            }
+        }
+    }
+
+    public boolean find(String firstName, String lastName) {
+        for (Employee employee : employees) {
+            if (Objects.equals(employee.getFirstName(), firstName) &&
+                    Objects.equals(employee.getLastName(), lastName)) {
+                return true;
+            }
+        }
+        return false;
+    }
 }
