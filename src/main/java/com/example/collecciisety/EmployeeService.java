ebef52bcf45881d5fd668ad570b7de03a4742ded
abdullinru <abdullin17021989@gmail.com>
2022-07-19 22:54:30 +0600

    добавил библиотеку StringUtils и использовал класс для проверки вводимой информации

--- src/main/java/com/example/collecciisety/EmployeeService.java
+++ src/main/java/com/example/collecciisety/EmployeeService.java
@@ -1,5 +1,6 @@
 package com.example.collecciisety;
 
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Service;
 
 import java.util.*;
@@ -23,28 +24,39 @@ public class EmployeeService {
     }
 
     public Employee add(String firstName, String lastName, int department, double salary) {
-        String key = getKey(firstName, lastName);
-        if (findPrivate(firstName, lastName)) {
+        if (StringUtils.isBlank(lastName) || StringUtils.isBlank(firstName) ||
+        StringUtils.contains(firstName, "\\d") || StringUtils.contains(lastName, "\\d")||
+        StringUtils.contains(firstName,"\\W") ||StringUtils.contains(lastName, "\\W")) {
+            throw new EmployeeNotEntered("Вы не ввели имя или фамилию сотрудника");
+        }
+        String key = getKey(StringUtils.capitalize(firstName), StringUtils.capitalize(lastName));
+        if (findPrivate(StringUtils.capitalize(firstName), StringUtils.capitalize(lastName))) {
             throw new EmployeeAlreadyAddedException("Данный сотрудник уже добавлен! ");
         }
         if (employees.size() == MAX) {
             throw new EmployeeStorageIsFullException("Массив переполнен! ");
         }
-        employees.put(key ,new Employee(firstName, lastName, department, salary));
+        employees.put(key ,new Employee(StringUtils.capitalize(firstName), StringUtils.capitalize(lastName), department, salary));
         return employees.get(key);
     }
 
     public Employee delete(String firstName, String lastName) {
-        if (!findPrivate(firstName, lastName)) {
+        if (StringUtils.isBlank(lastName) || StringUtils.isBlank(firstName)) {
+            throw new EmployeeNotEntered("Вы не ввели имя или фамилию сотрудника");
+        }
+        if (!findPrivate(StringUtils.capitalize(firstName), StringUtils.capitalize(lastName))) {
             throw new EmployeeNotFoundException("Нет такого сотрудника");
         } else {
-            return employees.remove(getKey(firstName, lastName));
+            return employees.remove(getKey(StringUtils.capitalize(firstName), StringUtils.capitalize(lastName)));
         }
     }
 
     public Employee find(String firstName, String lastName) {
-        if (findPrivate(firstName, lastName)) {
-            return employees.get(getKey(firstName, lastName));
+        if (StringUtils.isBlank(lastName) || StringUtils.isBlank(firstName)) {
+            throw new EmployeeNotEntered("Вы не ввели имя или фамилию сотрудника");
+        }
+        if (findPrivate(StringUtils.capitalize(firstName), StringUtils.capitalize(lastName))) {
+            return employees.get(getKey(StringUtils.capitalize(firstName), StringUtils.capitalize(lastName)));
         } else {
             throw new EmployeeNotFoundException("Сотрудник не найден");
         }
